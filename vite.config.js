7255ad83baa8c005849d723c1e43a2149b306388
Hemant Dubey <dubeyhemantjpr@gmail.com>
2024-11-08 20:26:37 +0530

    config upadte

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Certificate-Verification-System/",
+  base: "/Certificate_Verification/",
   // base: "/",
   plugins: [react()],
   resolve: {
